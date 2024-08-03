00d75c13d264f9220b128c55f6d6df9ad9e315cd
x <x@xpc>
2024-08-04 00:24:11 +0700

    fix vlist

--- src/pages/OrderList/index.tsx
+++ src/pages/OrderList/index.tsx
@@ -1,11 +1,15 @@
 import OrderItem from '@/components/OrderItem'
+import WebApp from '@twa-dev/sdk'
 import { InfiniteScroll, List, Tabs } from 'antd-mobile'
-import { CSSProperties, useState } from 'react'
+import { CSSProperties, useEffect, useRef, useState } from 'react'
 import { FixedSizeList as ListType } from 'react-window'
 
 export default function OrderListPage() {
 	const [hasMore] = useState(false)
 	const [orderList] = useState<number[]>(Array.from({ length: 100 }, (_, i) => i))
+	const listRef = useRef<HTMLDivElement>(null)
+	const [height, setHeight] = useState(0)
+	const [width, setWidth] = useState(0)
 
 	const onTabChange = (key: string) => {
 		console.log(key)
@@ -16,6 +20,15 @@ export default function OrderListPage() {
 	const rowRender = ({ index, style }: { index: number; style: CSSProperties }) => {
 		return <OrderItem style={style} info={index} />
 	}
+
+	useEffect(() => {
+		console.log(WebApp.viewportHeight)
+		console.log(WebApp.viewportStableHeight)
+		if (listRef.current) {
+			setHeight(listRef.current.clientHeight)
+			setWidth(listRef.current.clientWidth)
+		}
+	}, [listRef])
 
 	return (
 		<div className='flex-1 overflow-hidden flex flex-col'>
@@ -35,7 +48,7 @@ export default function OrderListPage() {
 				<Tabs.Tab title='待收货' key='2'></Tabs.Tab>
 				<Tabs.Tab title='已完成' key='3'></Tabs.Tab>
 			</Tabs>
-			<div className='flex-1 overflow-y-auto hide-scrollbar'>
+			<div ref={listRef} className='flex-1 overflow-y-auto hide-scrollbar'>
 				<List
 					style={
 						{
@@ -45,8 +58,8 @@ export default function OrderListPage() {
 					}
 				>
 					<ListType
-						height={500}
-						width={384}
+						height={height}
+						width={width}
 						itemSize={230}
 						className='hide-scrollbar secondary'
 						style={
